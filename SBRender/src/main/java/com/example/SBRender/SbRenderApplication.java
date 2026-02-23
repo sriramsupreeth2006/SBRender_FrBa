96115d3902992265da857e28336111aef93134c7
sriramsupreeth2006 <sriram.supreethd@gmail.com>
2026-02-23 15:10:55 +0530

    First Commit backend

---
+++ SBRender/src/main/java/com/example/SBRender/SbRenderApplication.java
@@ -0,0 +1,13 @@
+package com.example.SBRender;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbRenderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SbRenderApplication.class, args);
+    }
+
+}
---
+++ SBRender/src/test/java/com/example/SBRender/SbRenderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SBRender;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbRenderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
